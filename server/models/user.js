cdc13dc8e35098e67e75bf96f642c8ad6bcd6bf7
Oscar Rojas <rojascruzoscar@gmail.com>
2020-08-08 18:00:59 -0500

    Creating model of user
    - Created the model of the user
    - Created the connection to the database
    - Added hash security type to the password
    - Not returning the password to the user
    - POST method complete

---
+++ server/models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let validRoles = {
+	values: ['ADMIN_ROLE', 'USER_ROLE'],
+	errorMessage: '{VALUE} rol no esta definido, ingresa uno adecuado', //el que ingresa + mensaje
+};
+
+let Schema = mongoose.Schema;
+
+let schemaUser = new Schema({
+	name: {
+		type: String,
+		required: [true, 'The name is required jaja'],
+	},
+	email: {
+		type: String,
+		required: [true, 'The email is necessary'],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, 'Password is mandatory'],
+	},
+	img: {
+		type: String,
+		required: false,
+	},
+	role: {
+		type: String,
+		default: 'USER_ROLE',
+		enum: validRoles,
+	},
+	status: {
+		type: Boolean,
+		default: true,
+	},
+	google: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+schemaUser.methods.toJSON = function () {
+	let funcUser = this;
+	let userObject = funcUser.toObject();
+	delete userObject.password;
+	return userObject;
+};
+
+schemaUser.plugin(uniqueValidator, {
+	message: '{PATH} debe de ser único',
+});
+
+module.exports = mongoose.model('User', schemaUser);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 require('./config/config'); // Lee este archivo y ejecuta todo lo que contenga
 
 const express = require('express');
-const server = express();
+const mongoose = require('mongoose');
 
+const server = express();
 const bodyParser = require('body-parser');
 
 /*------Estos son middlewares ----------------*/
@@ -12,34 +13,12 @@ server.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 server.use(bodyParser.json());
 
-server.get('/users', (req, res) => {
-	res.json('get user');
-});
+// para usar las rutas que separamos al otro archivo
+server.use(require('./routes/user-routes'));
 
-server.post('/users', (req, res) => {
-	let bodyOfRequest = req.body;
-
-	if (bodyOfRequest.name === undefined) {
-		res.status(400).json({
-			ok: false,
-			description: 'El nombre es necesario',
-		}); //bad request
-	} else {
-		res.json({
-			person: bodyOfRequest,
-		});
-	}
-});
-
-server.put('/users/:id', (req, res) => {
-	let id = req.params.id;
-	res.json({
-		id,
-	});
-});
-
-server.delete('/users', (req, resp) => {
-	resp.json('INhabilitar usuarios');
+mongoose.connect('mongodb://localhost:27017/cafe', (error, resp) => {
+	if (error) throw error;
+	console.log('Base de datos online !');
 });
 
 server.listen(process.env.PORT, () => {
