f0a1a4dd308bb0adf1ed1d43133a48f54f8a77ab
Oscar Rojas <rojascruzoscar@gmail.com>
2020-08-17 15:59:54 -0500

    Mongo environment variable for connection

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
 	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-	urlDB = 'mongodb+srv://root:fzFQd7GaWJbfjSF6@cluster0.r8cab.mongodb.net/cafe';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
